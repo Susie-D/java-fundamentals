eb2e3dd28440bfde878cd683ab5b7be9b88df80a
Susie Douang <soutsada.d@gmail.com>
2024-10-30 15:59:06 -0500

    reverse a string w/ for loop

--- src/main/java/strings/TextProcessor.java
+++ src/main/java/strings/TextProcessor.java
@@ -5,7 +5,8 @@ package strings;
 public class TextProcessor {
 
     public static void main (String[] args) {
-        countWords("I love Java");
+        // countWords("I love Java");
+        reverseString("Hello World");
     }
 
     public static void countWords(String text) {
@@ -19,5 +20,11 @@ public class TextProcessor {
             System.out.println(word);
         }
 
+    }
+
+    public static void reverseString(String text) {
+        for (var i = text.length()-1; i >= 0; i--) {
+            System.out.print(text.charAt(i));
+        }
     }
 }
