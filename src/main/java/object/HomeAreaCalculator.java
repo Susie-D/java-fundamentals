529edf16f6d8638c2428514eda24b4716ff5a46f
Susie Douang <soutsada.d@gmail.com>
2024-10-30 14:29:42 -0500

    class instantiation, function method calls, and calculate input values

--- src/main/java/object/HomeAreaCalculator.java
+++ src/main/java/object/HomeAreaCalculator.java
@@ -1,19 +1,42 @@
 package object;
+import java.util.Scanner;
+
 
 public class HomeAreaCalculator {
+   private Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        Rectangle room1 = new Rectangle();
-        room1.setLength(50);
-        room1.setWidth(25);
+//        Rectangle room1 = new Rectangle();
+//        room1.setLength(50);
+//        room1.setWidth(25);
+//
+//        double areaOfRoom1 = room1.calculateArea();
+//
+//        Rectangle room2 = new Rectangle(30, 75);
+//        double areaOfRoom2 =  room2.calculateArea();
+//
+//        double totalArea = areaOfRoom1 + areaOfRoom2;
 
-        double areaOfRoom1 = room1.calculateArea();
+        HomeAreaCalculator calculator = new HomeAreaCalculator();
 
-        Rectangle room2 = new Rectangle(30, 75);
-        double areaOfRoom2 =  room2.calculateArea();
+        Rectangle room1 = calculator.getRoom();
+        Rectangle room2 = calculator.getRoom();
 
-        double total = areaOfRoom1 + areaOfRoom2;
-        System.out.println("Total area: " + total);
+        double totalArea = calculator.calculateAreaOfRoom(room1, room2);
+
+        System.out.println("Total area: " + totalArea);
     }
 
+    public Rectangle getRoom() {
+        System.out.println("Enter the length of the room");
+        double length = scanner.nextDouble();
+        System.out.println("Enter the width of the room");
+        double width = scanner.nextDouble();
+
+        return new Rectangle(length, width);
+    }
+
+    public double calculateAreaOfRoom(Rectangle room1, Rectangle room2) {
+        return (room1.calculateArea() + room2.calculateArea());
+    }
 }
